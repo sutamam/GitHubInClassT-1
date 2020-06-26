71b352cae032b6bfd7ff0bb5c8eb4fb987232be5
Simon <sli8@usf.edu>
2020-06-26 18:37:49 -0400

    Added message

--- GitHubInClassT-1/Program.cs
+++ GitHubInClassT-1/Program.cs
@@ -8,7 +8,9 @@ namespace GitHubInClassT_1
         {
             Console.WriteLine("Hello World!");
             
-            Console.WriteLine("Hello This is Shiva <<<<<<>>>>>>>>>-- Commenting on Sutama Work - Group-1"); 
+            Console.WriteLine("Hello This is Shiva <<<<<<>>>>>>>>>-- Commenting on Sutama Work - Group-1");
+
+            Console.WriteLine("Hello, this is Simon");
         }
     }
 }
